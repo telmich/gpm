8049288204060a618c0ef8f90801dc15558bf5b7
Nico Schottelius <nico@hydrogenium.schottelius.org>
2006-08-27 15:00:35 +0200

    Cleanup autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+# Nico Schottelius <nico-gpm /-at--/ schottelius.org>
+# 2006-08-27
+#
 
+set -e
 echo "Running autotools ..."
-
-aclocal -I m4 || exit 1
-autoheader || exit 1
-autoconf || exit 1
-
+aclocal -I m4
+autoheader
+autoconf
 echo "Done!  Now just run ./configure"
