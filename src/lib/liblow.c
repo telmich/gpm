59cd2ae63c21f3ddcd39e2a38aa24b0f5859485c
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-05-17 01:57:36 +0200

    Added flag patch:
    Date: Thu, 17 May 2007 01:46:44 +0200
    From: Samuel Thibault <samuel.thibault@ens-lyon.org>
    Subject: Re: [gpm] gpm-1.20.2-broken
    
    Hi,
    
    Samuel Thibault, le Thu 17 May 2007 01:22:22 +0200, a écrit :
    > As I reported some time ago, default handlers don't work anymore with
    > version 1.20.  Could you apply the attached patch for making them work
    > again?
    
    Mm, that one should be better.
    
                                              Samuel

--- src/lib/liblow.c
+++ src/lib/liblow.c
@@ -3,7 +3,7 @@
  *
  * Copyright 1994,1995   rubini@linux.it (Alessandro Rubini)
  * Copyright (C) 1998    Ian Zimmerman <itz@rahul.net>
- * Copyright 2001        Nico Schottelius (nico-gpm@schottelius.org)
+ * Copyright 2001-2007   Nico Schottelius (nico-gpm@schottelius.org)
  * Copyright 2004        Dmitry Torokhov <dtor@mail.ru>
  *
  * xterm management is mostly by jtklehto@stekt.oulu.fi (Janne Kukonlehto)
@@ -363,7 +363,7 @@ int Gpm_Open(Gpm_Connect *conn, int flag)
          }
          memcpy(tty, console_name, strlen(console_name) - 1);
          sprintf(&tty[strlen(console_name) - 1], "%i", flag);
-      } else { /* use your current vc */
+      } else if (flag==0) { /* use your current vc */
          if (isatty(0)) tty = ttyname(0);             /* stdin */
          if (!tty && isatty(1)) tty = ttyname(1);     /* stdout */
          if (!tty && isatty(2)) tty = ttyname(2);     /* stderr */
@@ -380,7 +380,8 @@ int Gpm_Open(Gpm_Connect *conn, int flag)
          }
 
          conn->vc = atoi(&tty[strlen(console_name) - 1]);
-      }
+      } else /* a default handler -- use console */
+        tty = strdup(console_name);
 
       if (gpm_consolefd == -1) {
          if ((gpm_consolefd = open(tty, O_WRONLY)) < 0) {
