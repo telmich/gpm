b7accc7dc27866271fa862a0f56fd2395c70cf2b
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-02-13 15:04:31 +0100

    add new daemon header

---
+++ src/headers/daemon.h
@@ -0,0 +1,53 @@
+/*
+ * Daemon internals
+ *
+ * Copyright (c) 2008    Nico Schottelius <nico-gpm2008 at schottelius.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _GPM_DAEMON_H
+#define _GPM_DAEMON_H
+
+/*************************************************************************
+ * Types
+ */
+
+struct options {
+   int autodetect;            /* -u [aUtodetect..'A' is not available] */
+   int no_mice;               /* number of mice */
+   int repeater;              /* repeat data */
+   char *repeater_type;       /* repeat data as which mouse type */
+   int run_status;            /* startup/daemon/debug */
+   char *progname;            /* hopefully gpm ;) */
+   struct micetab *micelist;  /* mice and their options */
+   char *consolename;         /* /dev/tty0 || /dev/vc/0 */
+};
+
+/*************************************************************************
+ * Global variables
+ */
+
+extern int opt_resize;        /* not really an option */
+extern struct options option; /* one should be enough for us */
+
+/*************************************************************************
+ * Functions
+ */
+
+void gpm_killed(int signo);
+
+
+#endif
