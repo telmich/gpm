730db5e00b8770fa317b1bdf16f488172251fa8c
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-02-13 16:02:00 +0100

    add headers to open_console

--- src/daemon/open_console.c
+++ src/daemon/open_console.c
@@ -19,24 +19,35 @@
  *
  ********/
 
-#include "headers/message.h"        /* messaging in gpm */
-#include "headers/daemon.h"         /* daemon internals */
+#include <fcntl.h>                  /* open and co.      */
+#include <sys/stat.h>               /* stat()            */
+#include <stropts.h>                /* ioctl             */
+
+/* Linux specific (to be outsourced in gpm2 */
+#include <linux/serial.h>           /* for serial console check */
+#include <asm/ioctls.h>            /* for serial console check */
+
+
+#include "headers/message.h"        /* messaging in gpm  */
+#include "headers/daemon.h"         /* daemon internals  */
 
 static inline int open_console(const int mode)
 {
-   int fd;
-   struct stat sb;
-   int maj, twelve=12;
+   int                  fd;
+   int                  maj;
+   int                  twelve = 12;
    struct serial_struct si;
+   struct stat          sb;
 
    fd = open(option.consolename, mode);
    if (fd != -1) {
       fstat(fd, &sb);
       maj = major(sb.st_rdev);
       if (maj != 4 && (maj < 136 || maj > 143)) {
-          if (ioctl (fd, TIOCLINUX, &twelve) < 0) {
-              if (si.line > 0)
+          if (ioctl(fd, TIOCLINUX, &twelve) < 0) {
+              if (si.line > 0) {
                   gpm_report(GPM_PR_OOPS,GPM_MESS_OPEN_SERIALCON);
+               }
           }
       }
    return fd;
