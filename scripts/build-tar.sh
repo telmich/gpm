f5bb8d16d24a25b05d2d32a2ca7478a91cd97d25
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-02-16 15:14:40 +0100

    remove -

--- scripts/build-tar.sh
+++ scripts/build-tar.sh
@@ -3,4 +3,4 @@
 version=1.20.3pre3
 
 git archive --format=tar --prefix=gpm-${version}/ HEAD | \
-   tee ../gpm--${version}.tar | bzip2 -9 > ../gpm--${version}.tar.bz2
+   tee ../gpm-${version}.tar | bzip2 -9 > ../gpm-${version}.tar.bz2
