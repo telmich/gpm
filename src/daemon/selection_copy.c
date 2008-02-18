2ab1aac9e5bbe36c65d0fc6439425d81410663f6
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-02-18 19:13:23 +0100

    next to finish splitoff

--- src/daemon/selection_copy.c
+++ src/daemon/selection_copy.c
@@ -22,4 +22,35 @@
 #include "headers/message.h"        /* messaging in gpm */
 #include "headers/daemon.h"         /* daemon internals */
 
+static inline void selection_copy(int x1, int y1, int x2, int y2, int mode)
+{
+/*
+ * The approach in "selection" causes a bus error when run under SunOS 4.1
+ * due to alignment problems...
+ */
+   unsigned char buf[6*sizeof(short)];
+   unsigned short *arg = (unsigned short *)buf + 1;
+   int fd;
+
+   buf[sizeof(short)-1] = 2;  /* set selection */
+
+   arg[0]=(unsigned short)x1;
+   arg[1]=(unsigned short)y1;
+   arg[2]=(unsigned short)x2;
+   arg[3]=(unsigned short)y2;
+   arg[4]=(unsigned short)mode;
+
+   if ((fd=open_console(O_WRONLY))<0)
+      gpm_report(GPM_PR_OOPS,GPM_MESS_OPEN_CON);
+   /* FIXME: should be replaced with string constant (headers/message.h) */
+   gpm_report(GPM_PR_DEBUG,"ctl %i, mode %i",(int)*buf, arg[4]);
+   if (ioctl(fd, TIOCLINUX, buf+sizeof(short)-1) < 0)
+     gpm_report(GPM_PR_OOPS,GPM_MESS_IOCTL_TIOCLINUX);
+   close(fd);
+
+   if (mode < 3) {
+      opt_aged = 0;
+      last_selection_time = time(0);
+   }
+}
 
