caa80dbed3d7c36303d6aee577d7f74b12d53619
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-02-22 20:21:25 +0100

    use $1 for building the release tar

--- scripts/build-tar.sh
+++ scripts/build-tar.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-version=1.20.3pre3
+version=$1
+
+[ "$version" ] || exit 3
 
 git archive --format=tar --prefix=gpm-${version}/ HEAD | \
    tee ../gpm-${version}.tar | bzip2 -9 > ../gpm-${version}.tar.bz2
