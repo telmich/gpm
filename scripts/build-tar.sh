33072f5d329f26329a8e1779fdbc6672010f1c0f
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-03-14 12:25:53 +0100

    update scripts/build-tar.sh to use the dirname

--- scripts/build-tar.sh
+++ scripts/build-tar.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
+# Nico Schottelius
+# GPLv3
+# 20080314
 
 version=$1
 
 [ "$version" ] || exit 3
 
-git archive --format=tar --prefix=gpm-${version}/ HEAD | \
-   tee ../gpm-${version}.tar | bzip2 -9 > ../gpm-${version}.tar.bz2
+if [ ! -d .git ]; then
+   echo noooooo.
+   exit 1
+fi
+
+pwd=$(pwd)
+name=${pwd##*/}
+
+git archive --format=tar --prefix=${name}-${version}/ HEAD | \
+   tee ../${name}-${version}.tar | bzip2 -9 > ../${name}-${version}.tar.bz2
