21f782d9ff1ff36a3cb0f3c1003749b039865001
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-03-27 12:33:43 +0100

    add header to splitoff files

--- src/drivers/ps2/m.c
+++ src/drivers/ps2/m.c
@@ -1,3 +1,27 @@
+/*
+ * general purpose mouse (gpm)
+ *
+ * Copyright (c) 2008        Nico Schottelius <nico-gpm2008 at schottelius.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ ********/
+
+#include "types.h"                  /* Gpm_type         */
+
+
 static int M_ps2(Gpm_Event *state,  unsigned char *data)
 {
    static int tap_active=0; /* there exist glidepoint ps2 mice */
