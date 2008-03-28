48f4613f513a35dbf5ab578352e2a4a82ddbc5d8
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-03-28 13:39:46 +0100

    move parts of gpmInt.h to mice.h

--- src/headers/mice.h
+++ src/headers/mice.h
@@ -36,6 +36,30 @@
 #define GPM_B_BOTH (GPM_B_LEFT|GPM_B_RIGHT)
 #define REALPOS_MAX 16383 /*  min 0 max=16383, but due to change.  */
 
+/*** mouse commands ***/ 
+#define GPM_AUX_SEND_ID    0xF2
+#define GPM_AUX_ID_ERROR   -1
+#define GPM_AUX_ID_PS2     0
+#define GPM_AUX_ID_IMPS2   3
+
+/* these are shameless stolen from /usr/src/linux/include/linux/pc_keyb.h    */
+#define GPM_AUX_SET_RES        0xE8  /* Set resolution */
+#define GPM_AUX_SET_SCALE11    0xE6  /* Set 1:1 scaling */ 
+#define GPM_AUX_SET_SCALE21    0xE7  /* Set 2:1 scaling */
+#define GPM_AUX_GET_SCALE      0xE9  /* Get scaling factor */
+#define GPM_AUX_SET_STREAM     0xEA  /* Set stream mode */
+#define GPM_AUX_SET_SAMPLE     0xF3  /* Set sample rate */ 
+#define GPM_AUX_ENABLE_DEV     0xF4  /* Enable aux device */
+#define GPM_AUX_DISABLE_DEV    0xF5  /* Disable aux device */
+#define GPM_AUX_RESET          0xFF  /* Reset aux device */
+#define GPM_AUX_ACK            0xFA  /* Command byte ACK. */ 
+
+#define GPM_AUX_BUF_SIZE    2048  /* This might be better divisible by
+                                 three to make overruns stay in sync
+                                 but then the read function would need
+                                 a lock etc - ick */
+                                                      
+
 
 /*************************************************************************
  * Global variables
