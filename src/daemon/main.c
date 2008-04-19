d8a19d9d9c5aa2c85709958aa8e803a9cf29e686
Mike Frysinger <vapier@gentoo.org>
2008-04-19 04:12:01 -0400

    pull in headers/daemon.h for prototypes
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- src/daemon/main.c
+++ src/daemon/main.c
@@ -24,7 +24,7 @@
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  ********/
 
-
+#include "headers/daemon.h"
 #include "headers/gpmInt.h"
 
 int main(int argc, char **argv)
