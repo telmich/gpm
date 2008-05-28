38838c63cbdae1d531504d4ceeb715ce39eb5cfb
Jonathan Nieder <jrnieder@uchicago.edu>
2008-05-28 14:20:25 -0500

    Demote message when the daemon is killed to debugging output
    Included in Debian gpm 1.20.3~pre3-1
    under the name debian/patches/030_daemon_quit
    
    It is expected for a daemon to be killed from time to time
    (for upgrades, for example), so it is inappropriate to notify
    a user of this with a message of priority LOG_INFO. The
    priority LOG_DEBUG is more appropriate.
    
    Another victim of gpn.c refactoring

--- src/daemon/check_kill.c
+++ src/daemon/check_kill.c
@@ -53,7 +53,7 @@ void check_kill(void)
    if (kill(old_pid,SIGTERM) == -1)
       gpm_report(GPM_PR_OOPS,GPM_MESS_CANT_KILL, old_pid);
 
-   gpm_report(GPM_PR_INFO,GPM_MESS_KILLED,old_pid);
+   gpm_report(GPM_PR_DEBUG,GPM_MESS_KILLED,old_pid);
    exit(0);
 }
 
