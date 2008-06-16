062990c19a632b08b543e5baea0268aa4a0da049
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-06-16 16:14:49 +0200

    fix tar dependency
    Signed-off-by: Nico Schottelius <nico@denkbrett.schottelius.org>

--- autogen.sh
+++ autogen.sh
@@ -23,5 +23,4 @@ echo "${date}"    > .releasedate
 ${ACLOCAL-aclocal} -I config
 ${LIBTOOLIZE-libtoolize} --force --copy
 ${AUTOHEADER-autoheader}
-${AUTOMAKE-automake} --add-missing --copy 2> /dev/null || true
 ${AUTOCONF-autoconf}
