a9306b5365670c26f60cf98e9914fc30209fe5ae
Nico Schottelius <nico@kr.ethz.ch>
2010-05-28 10:09:17 +0200

    add ps/2 handler
    Signed-off-by: Nico Schottelius <nico@kr.ethz.ch>

---
+++ gpm2/protocols/ps2/gpm2-ps2.c
@@ -0,0 +1,42 @@
+/*
+ *   Copyright (c) 2010 Nico Schottelius <nico-gpm2 () schottelius.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *
+ * Handle ps2 as described on http://www.computer-engineering.org/ps2mouse/
+ *
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+   char enable_data_reporting = 0xf4;
+
+   /* ps2 has three bytes per movement packet */
+   char input[3];
+
+   write(STDOUT_FILENO, &enable_data_reporting, sizeof(enable_data_reporting));
+
+   while(1) {
+      read(STDIN_FILENO, input, sizeof(input));
+
+      fprintf(stderr, "x = %d, y = %d\n", input[1], input[2]);
+
+   }
+
+}
