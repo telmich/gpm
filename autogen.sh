3cef66890e730db0165132865feef5f3164c6304
Nico Schottelius <nico@brief.schottelius.org>
2012-10-26 21:26:05 +0200

    get version string from git without calling ugly m4 macros

--- autogen.sh
+++ autogen.sh
@@ -1,24 +1,25 @@
 #!/bin/sh -e
 
-set -x
-
 [ -d config ] || mkdir config
 
-#
-# Generate version in m4 format, because the following two in acinclude.m4
-# do not work:
-# 
-# revgit="`cat $srcdir/.gitversion`"
-# m4_define([AC_PACKAGE_VERSION], [$revgit])
-# 
-# and
-# m4_define([AC_PACKAGE_VERSION], include(.gitversion))
-#
-version="$(git describe)"
-date="$(date +%Y%m%d\ %T\ %z)"
-echo "define([AC_PACKAGE_VERSION], [${version} ${date}])" > .gitversion.m4
-echo "${version}" > .gitversion
-echo "${date}"    > .releasedate
+version=$(git describe 2>/dev/null)
+
+# No git? use changelog information
+if [ -z "$version" ]; then
+    version=$(grep '^[[:digit:]]' doc/changelog | head -n1 | cut -d: -f1)
+    date=$(grep '^[[:digit:]]' doc/changelog | head -n1 | cut -d: -f2)
+else
+    date=$(git log -1 --pretty="format:%ai" "$version")
+fi
+
+cat << eof > configure.ac
+AC_INIT([gpm],[$version],[http://www.nico.schottelius.org/software/gpm/])
+
+releasedate="$date"
+release="$version"
+eof
+
+cat configure.ac.footer >> configure.ac
 
 ${ACLOCAL-aclocal} -I config
 ${LIBTOOLIZE-libtoolize} -n --install 2>/dev/null && LIBTOOL_FLAGS="--install" || LIBTOOL_FLAGS=""
